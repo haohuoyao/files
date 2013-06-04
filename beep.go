6444dbf22a14bb63f7c6cf45cbe1d93400409937
haohuoyao <haohuoyao@gmail.com>
2013-06-04 09:07:50 +0800

    Update beep.go

--- beep.go
+++ beep.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	a:=flag.Int("a",500,"")
-	b:=flag.Int("b",3000,"")
+	a:=flag.Int("a",1000,"")
+	b:=flag.Int("b",2000,"") // two seconds
 	flag.Parse()
 	k := syscall.MustLoadDLL("kernel32.dll")
 	k.MustFindProc("Beep").Call(uintptr(*a),uintptr(*b))
